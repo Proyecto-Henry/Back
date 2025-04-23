fe814bd515251f3e9c7f8ca172bbeea5e8f834c3
Smile <kevinschn20@gmail.com>
2025-04-23 11:30:07 -0300

    nombre carpeta Middleware arreglado

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { loggerGlobal } from './middelwares/logger.middelware';
+import { loggerGlobal } from './middlewares/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
