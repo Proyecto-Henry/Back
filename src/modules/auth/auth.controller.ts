10d6654920d4b72ccf523c344dbced174ff4a21b
cristian <cristian-mansilla@live.com.ar>
2025-04-24 15:16:44 -0300

    ruta signIn y signUp para admin

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpAuthDto } from './dtos/signup-auth.dto';
 import { loginAuthDto } from './dtos/signin-auth.dto';
+import { AdminDto } from './dtos/admin-auth.dto';
 import { sign } from 'crypto';
 
 @Controller('auth')
@@ -17,6 +18,16 @@ export class AuthController {
   async signUp(@Body() signUpUser: SignUpAuthDto) {
     const newUser = await this.authService.signUp(signUpUser);
     return this.authService.signUp(signUpUser);
+  }
+
+  @Post('signInAdmin')
+  signInAdmin(@Body() signInAdmin: AdminDto) {
+    return this.authService.signInAdmin(signInAdmin);
+  }
+
+  @Post('signUpAdmin')
+  signUpAdmin(@Body() signUpAdmin: AdminDto) {
+    return this.authService.signUpAdmin(signUpAdmin);
   }
 
   @Post('test')
