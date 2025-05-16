534a5fd2a819c04c121c54942ebe837d3b01cecc
cristian <cristian-mansilla@live.com.ar>
2025-05-16 14:30:15 -0300

    editado mensaje por consola del super admin

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -21,7 +21,7 @@ export const config = {
   // },
   entities: ['dist/**/*.entity{.js,.ts}'],
   migrations: ['dist/migrations/*{.js,.ts}'],
-  dropSchema: true,
+  dropSchema: false,
 };
 
 export default registerAs('typeorm', () => config);
