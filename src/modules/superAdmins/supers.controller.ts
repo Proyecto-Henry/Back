c20880f9dd27f37103a831b799bbdcaeccbaee0f
cristian <cristian-mansilla@live.com.ar>
2025-05-16 14:54:52 -0300

    endpoint get super admins

--- src/modules/superAdmins/supers.controller.ts
+++ src/modules/superAdmins/supers.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { SuperAdminService } from "./supers.service";
 import { SuperAdminDto } from "./dtos/supers.dto";
+import { Super_Admin } from "src/entities/Super_Admin.entity";
 
 @Controller('super') 
 export class SuperAdminController {
@@ -8,6 +9,11 @@ export class SuperAdminController {
     constructor(
         private readonly superAdminService: SuperAdminService
     ){}
+
+    @Get()
+    getSuperAdmin(): Promise<Omit<Super_Admin,'password'>[]> {
+        return this.superAdminService.getSuperAdmin();
+    }
 
     @Post('admin')
     registerSuperAdmin(@Body() data: SuperAdminDto) {
